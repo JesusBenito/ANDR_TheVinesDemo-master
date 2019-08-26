15d80c53d03be986a2c90844610fed7facb082b4
jesusbenito <ing.fernandobenito@gmail.com>
2019-08-26 08:53:40 -0500

    version 1.0.3

--- app/src/main/java/com/vlim/deimos/CavasActivity.java
+++ app/src/main/java/com/vlim/deimos/CavasActivity.java
@@ -1,13 +1,16 @@
 package com.vlim.deimos;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class CavasActivity extends AppCompatActivity {
 
-    Button btn_ver;
+    ImageView btn_regresar;
     boolean bandera;
     ImageView imageView;
 
@@ -15,6 +18,13 @@ public class CavasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cavas);
+        btn_regresar=findViewById(R.id.btn_regresar);
+        btn_regresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
 
     }
 }
--- app/src/main/java/com/vlim/deimos/MainActivity.java
+++ app/src/main/java/com/vlim/deimos/MainActivity.java
@@ -18,12 +18,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        //setSupportActionBar(toolbar);
+        //getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         btn_buscar=findViewById(R.id.btn_buscar);
         btn_ernesto=findViewById(R.id.btn_ernesto);
-
+        btn_ernesto.setClickable(true);
         btn_ernesto.setOnClickListener(this);
 
     }
