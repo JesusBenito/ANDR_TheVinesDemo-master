4c612995c6fa093a306c9f79106bc8508c727c67
jesusbenito <ing.fernandobenito@gmail.com>
2019-08-26 16:44:16 -0500

    version 1.0.4

--- app/src/main/java/com/vlim/deimos/MainActivity.java
+++ app/src/main/java/com/vlim/deimos/MainActivity.java
@@ -7,11 +7,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView btn_buscar;
     private ImageView btn_ernesto;
+    private SearchView simpleSearchView;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,24 +24,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //setSupportActionBar(toolbar);
         //getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        btn_buscar=findViewById(R.id.btn_buscar);
         btn_ernesto=findViewById(R.id.btn_ernesto);
+        simpleSearchView = findViewById(R.id.simpleSearchView);
+        imageView = findViewById(R.id.img_logo);
         btn_ernesto.setClickable(true);
         btn_ernesto.setOnClickListener(this);
+        simpleSearchView.setOnSearchClickListener(this);
+
 
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.btn_buscar:
-
-                break;
             case R.id.btn_ernesto:
                 Intent intent=new Intent(MainActivity.this, CavasActivity.class);
                 startActivity(intent);
                 break;
-
+            case R.id.simpleSearchView:
+                if(simpleSearchView.isClickable()) {
+                    imageView.setVisibility(View.GONE);
+                }else{
+                    imageView.setVisibility(View.VISIBLE);
+                }
         }
     }
 }
